19d224f35c2dfcafe01ed1cd3154538d0b8ae0ea
zackynson <crys.chb@hotmail.com>
2020-02-11 14:17:20 -0300

    last commit

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -5,32 +5,35 @@ class Post extends Component{
 
   state={
     id:null,
-    user:{name:'', avatar:''},
+    author:{name:'', avatar:''},
     date:'',
-    content:''
+    content:'',
+    comments:[]
   }
 
   componentDidMount(){
-    const {id, user, date, content} = this.props.data
-    this.setState({id,user,date,content})
+    const {id, author, date, content, comments} = this.props.data
+    this.setState({id,author,date,content,comments})
   }
 
   render (){
     return  (
       <div id="container">
         <div id="info">
-          <img className="avatar" src={this.state.user.avatar} alt={this.state.user.name}/>
+          <img className="avatar" src={this.state.author.avatar} alt={this.state.author.name}/>
           <div>
-           <h2>{this.state.user.name}</h2>
+           <h2>{this.state.author.name}</h2>
            <small>{this.state.date}</small>
           </div>
         </div>
       <p id="content">{this.state.content}</p>
       <div id="commentaries">
-        <div id="commentary">
-        <img className="avatar" src="https://blog.rocketseat.com.br/content/images/2019/05/profile.png" alt="" srcset=""/>
-          <p id="comentary-content"><strong id="username">Diego Fernandes</strong> A Rocketseat está sempre em busca de novos membros para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, pode me chamar no Discord! (Sério, me chamem mesmo, esse comentário é real)</p>
-        </div>
+      {this.state.comments?.map(comment => (
+      <div key={comment.id} id="commentary">
+        <img className="avatar" src={comment.author.avatar} alt={comment.author.name}/>
+          <p id="comentary-content"><strong id="username">{comment.author.name}</strong>{comment.content}</p>
+        </div>) )}
+        
       </div>
       </div>
     )
--- src/components/postlist/index.js
+++ src/components/postlist/index.js
@@ -1,32 +1,63 @@
 import React, { Component } from 'react';
 import Post from '../post/index'
+import './style.css'
 
 export default class postlist extends Component {
   state={
-    posts:[
+    posts:
+    [
       { 
         id:1,
-        user:{name:'Julio Alcantara', avatar:"https://mb.cision.com/Public/11571/2723198/94d70bece4bcac17_400x400ar.jpg"},
+        author:{name:'Julio Alcantara', avatar:"https://mb.cision.com/Public/11571/2723198/94d70bece4bcac17_400x400ar.jpg"},
         date:'04 jun 2019',
-        content:'Pessoal, alguém sabe se a rocketseat está contratando?'
-      }
+        content:'Pessoal, alguém sabe se a rocketseat está contratando?',
+        comments:[
+          {
+            id: 1,
+            author: {
+              name: "Diego Fernandes",
+              avatar: "https://blog.rocketseat.com.br/content/images/2019/05/profile.png"
+            },
+            content: "A Rocketseat está sempre em busca de novos membros para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, pode me chamar no Discord! (Sério, me chamem mesmo, esse comentário é real)"
+          }
+        ]
+      },
+      { 
+        id:2,
+        author:{name:'Gabriel Lisboa', avatar:"https://cdn.pixabay.com/photo/2019/05/24/10/59/business-man-4226005_960_720.jpg"},
+        date:'04 jun 2019',
+        content:'Fala galera, beleza? Estou fazendo o Bootcamp GoStack da Rocketseat e está sendo muito massa! Alguém mais aí fazendo, comenta na publicação para trocarmos uma ideia.',
+        comments:[
+          {
+            id: 1,
+            author: {
+              name: "Clara Lisboa",
+              avatar: "https://static01.nyt.com/images/2019/11/17/books/review/17Salam/Salam1-articleLarge.jpg?quality=75&auto=webp&disable=upscale"
+            },
+            content: "Também estou fazendo o Bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha API dos desafios contruída!"
+          },
+          {
+            id: 2,
+            author: {
+              name: "Cézar Toledo",
+              avatar: "https://www.royalfashionist.com.br/wp-content/uploads//2016/03/foto-SORRISO-LINEAR.jpg"
+            },
+            content: "Cézar Toledo Que maaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes!"
+          }
+        ]
+      },
     ]
   }
 
-
   render() {
     return (
-      <>
+      <div id="postlist">
         {this.state.posts.map(post => (
         <Post 
           key={post.id} 
           data={post} />
           ))}
-        
-
-      </>
+      </div>
     )
   }
 }
-
-content:'Pessoal, alguém sabe se a rocketseat está contratando?'
